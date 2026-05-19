719728bb12ac038fd6a97417daa0791f11f4d8de
NAOUI Yacine <naouiyassine009@gmail.com>
2026-05-20 00:22:58 +0100

    cm

--- app/db.php
+++ app/db.php
@@ -1,5 +1,5 @@
 <?php
-    $connexion = new mysqli("localhost", "root", "","tp_php");
+    $connexion = new mysqli("db", "root", "root","tp_php");
     if($connexion  ->connect_error){
         die("Connection erronée: " . $connexion->connect_error);
 }
